b5e3b5060f947a49060f500ab61ace0490e31f86
Martin Polden <martin.polden@gmail.com>
2014-03-14 19:27:56 +0100

    Use soft tabs in go-mode

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -4,4 +4,12 @@
 ;; load go-mode
 (require 'go-mode-load)
 
+;; soft tabs or death
+(add-hook 'go-mode-hook
+          (lambda ()
+            (setq
+              tab-width 4
+              tab-stop-list (number-sequence 4 120 4)
+              indent-tabs-mode nil)))
+
 (provide 'init-go-mode)
