0183b827c904f6ae400a15c6f6183efcc669fc69
Martin Polden <martin.polden@gmail.com>
2014-03-18 14:27:31 +0100

    Indent

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -8,8 +8,8 @@
 (add-hook 'go-mode-hook
           (lambda ()
             (setq
-              tab-width 4
-              tab-stop-list (number-sequence 4 120 4)
-              indent-tabs-mode nil)))
+             tab-width 4
+             tab-stop-list (number-sequence 4 120 4)
+             indent-tabs-mode nil)))
 
 (provide 'init-go-mode)
