ac8198f977030fc70226d6fd5a33ef2b1a125104
Martin Polden <martin.polden@gmail.com>
2014-03-24 09:42:00 +0100

    Use markdown-mode for .markdown files

--- lisp/init-markdown-mode.el
+++ lisp/init-markdown-mode.el
@@ -1,7 +1,8 @@
 ;; install package
 (require-package 'markdown-mode)
 
-;; use markdown mode for .md extensions
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+;; use markdown mode for .md and .markdown extensions
+(require 'markdown-mode)
+(add-to-list 'auto-mode-alist '("\\.(md|markdown)$" . markdown-mode))
 
 (provide 'init-markdown-mode)
