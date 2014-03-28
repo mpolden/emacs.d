22f142bbf5d384e03de6d3ad37372921d19437bb
Martin Polden <martin.polden@gmail.com>
2014-03-28 20:41:41 +0100

    Add json-mode

--- lisp/init-js2-mode.el
+++ lisp/init-js2-mode.el
@@ -1,9 +1,14 @@
-;; install package
+;; install packages
 (require-package 'js2-mode)
+(require-package 'json-mode)
 
 ;; load js2-mode
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;; load json-mode
+(require 'json-mode)
+(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
 
 ;; enable electric-indent-mode
 (add-hook 'js2-mode-hook
