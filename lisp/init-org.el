67ace6a185aef1fce317569081ee8b9b09c2afbc
Martin Polden <martin.polden@gmail.com>
2014-04-13 21:11:42 +0200

    Set default directory for org files

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -4,4 +4,9 @@
 ;; load org
 (require 'org)
 
+;; set default location for org files
+(let ((dirname "~/Dropbox/org"))
+  (if (file-directory-p dirname)
+      (setq org-directory dirname)))
+
 (provide 'init-org)
