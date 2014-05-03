8a8cc99f745a711495fbf6d90d78f0e9f3bde705
Martin Polden <martin.polden@gmail.com>
2014-05-03 13:22:47 +0200

    Add clj-refactor

--- lisp/init-clojure-mode.el
+++ lisp/init-clojure-mode.el
@@ -1,9 +1,16 @@
 ;; install packages
 (require-package 'clojure-mode)
 (require-package 'cider)
+(require-package 'clj-refactor)
 
-;; load clojure-mode and cider
 (require 'clojure-mode)
 (require 'cider)
+(require 'clj-refactor)
+
+;; clj-refactor-mode keybinding
+(cljr-add-keybindings-with-prefix "C-c C-m")
+
+;; enable clj-refactor-mode
+(add-hook 'clojure-mode-hook (lambda () (clj-refactor-mode 1)))
 
 (provide 'init-clojure-mode)
