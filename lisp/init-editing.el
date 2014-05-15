c53ddcfa28ba7b64cb44b38ab834d3500584e71f
Martin Polden <martin.polden@gmail.com>
2014-05-15 09:02:21 +0200

    Add whole-line-or-region

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -1,10 +1,27 @@
-;; install package
+;; install packages
 (require-package 'move-text)
+(require-package 'whole-line-or-region)
 
 (require 'move-text)
+(require 'whole-line-or-region)
 
-;; bind keys
-(global-set-key (kbd "<M-up>") 'move-text-up)
-(global-set-key (kbd "<M-down>") 'move-text-down)
+;; bind keys for moving lines up and down
+(global-set-key (kbd "M-<up>") 'move-text-up)
+(global-set-key (kbd "M-<down>") 'move-text-down)
+
+;; duplicating a line or region
+(defun duplicate-region (beg end)
+  "Insert a copy of the current region after the region."
+  (interactive "r")
+  (save-excursion
+    (goto-char end)
+    (insert (buffer-substring beg end))))
+
+(defun duplicate-line-or-region (prefix)
+  "Duplicate either the current line or any current region."
+  (interactive "*p")
+  (whole-line-or-region-call-with-region 'duplicate-region prefix t))
+
+(global-set-key (kbd "C-c p") 'duplicate-line-or-region)
 
 (provide 'init-editing)
