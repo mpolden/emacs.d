a8f084df529cb244c7d9f0129fbc559faae336bb
Martin Polden <martin.polden@gmail.com>
2014-05-16 09:10:15 +0200

    Configure open line above/below

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -31,4 +31,21 @@
 (global-set-key (kbd "M-n") 'forward-paragraph)
 (global-set-key (kbd "M-p") 'backward-paragraph)
 
+;; open line below or above current line
+(defun open-line-below ()
+  (interactive)
+  (end-of-line)
+  (newline)
+  (indent-for-tab-command))
+
+(defun open-line-above ()
+  (interactive)
+  (beginning-of-line)
+  (newline)
+  (forward-line -1)
+  (indent-for-tab-command))
+
+(global-set-key (kbd "C-o") 'open-line-below)
+(global-set-key (kbd "C-S-o") 'open-line-above)
+
 (provide 'init-editing)
