0e3742758ecaceae81280d7bbf9bf44979062530
Martin Polden <martin.polden@gmail.com>
2014-05-20 20:39:35 +0200

    Add magit

--- lisp/init-git-mode.el
+++ lisp/init-git-mode.el
@@ -1,13 +1,17 @@
 ;; install packages
+(require-package 'magit)
 (require-package 'git-commit-mode)
 (require-package 'git-rebase-mode)
 (require-package 'gitconfig-mode)
 (require-package 'gitignore-mode)
 
-;; load packages
+(require 'magit)
 (require 'git-commit-mode)
 (require 'git-rebase-mode)
 (require 'gitconfig-mode)
 (require 'gitignore-mode)
+
+;; magit keybinding
+(global-set-key (kbd "C-x m") 'magit-status)
 
 (provide 'init-git-mode)
