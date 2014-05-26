8b20967c4a38463db2abe47bd3fb2608fe20d554
Martin Polden <martin.polden@gmail.com>
2014-05-26 10:59:37 +0200

    M-w C-y does the same thing in WLR mode

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -12,21 +12,6 @@
 (global-set-key (kbd "M-<up>") 'move-text-up)
 (global-set-key (kbd "M-<down>") 'move-text-down)
 
-;; duplicating a line or region
-(defun duplicate-region (beg end)
-  "Insert a copy of the current region after the region."
-  (interactive "r")
-  (save-excursion
-    (goto-char end)
-    (insert (buffer-substring beg end))))
-
-(defun duplicate-line-or-region (prefix)
-  "Duplicate either the current line or any current region."
-  (interactive "*p")
-  (whole-line-or-region-call-with-region 'duplicate-region prefix t))
-
-(global-set-key (kbd "C-c p") 'duplicate-line-or-region)
-
 ;; undo
 (global-set-key (kbd "C-u") 'undo)
 
