d329f72c3a72890a622c2c6f85be5fce7ded7789
Martin Polden <martin.polden@gmail.com>
2014-06-04 11:06:46 +0200

    Reuse ag buffers and highlight search term

--- lisp/init-ag.el
+++ lisp/init-ag.el
@@ -3,6 +3,12 @@
 
 (require 'ag)
 
+;; reuse search results buffer
+(setq ag-reuse-buffers t)
+
+;; highlight search term in results
+(setq ag-highlight-search t)
+
 ;; keybinding
 (global-set-key (kbd "C-c a") 'ag-project)
 
