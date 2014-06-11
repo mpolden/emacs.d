7731cc997435346ce427f5ccd16a0533d3376ba0
Martin Polden <martin.polden@gmail.com>
2014-06-11 20:38:55 +0200

    Make ibuffer keybindings consistent with dired

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -9,7 +9,12 @@
 (add-hook 'ibuffer-hook
           (lambda ()
             (ibuffer-auto-mode)
-            (ibuffer-vc-set-filter-groups-by-vc-root)))
+            (ibuffer-vc-set-filter-groups-by-vc-root)
+            ;; make C-o and o behave as in dired
+            (define-key ibuffer-mode-map (kbd "o")
+              ibuffer-visit-buffer-other-window)
+            (define-key ibuffer-mode-map (kbd "C-o")
+              ibuffer-visit-buffer-other-window-noselect)))
 
 ;; use ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer-other-window)
