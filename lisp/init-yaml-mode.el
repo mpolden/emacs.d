72cca413254da54c29f792e1fd4ca6063b7f83c5
Martin Polden <martin.polden@gmail.com>
2014-06-11 20:52:43 +0200

    Disable electric-indent in yaml-mode

--- lisp/init-yaml-mode.el
+++ lisp/init-yaml-mode.el
@@ -4,4 +4,8 @@
 ;; load yaml-mode
 (require 'yaml-mode)
 
+;; disable electric indent
+(add-hook 'yaml-mode-hook
+          (lambda () (set (make-local-variable 'electric-indent-mode) nil)))
+
 (provide 'init-yaml-mode)
