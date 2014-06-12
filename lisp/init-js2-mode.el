0af39a4745dc455455703536e487cf3807bb26c4
Martin Polden <martin.polden@gmail.com>
2014-06-12 19:15:01 +0200

    Use \' to match end of string

--- lisp/init-js2-mode.el
+++ lisp/init-js2-mode.el
@@ -4,11 +4,11 @@
 
 ;; load js2-mode
 (require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 ;; load json-mode
 (require 'json-mode)
-(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
 
 ;; enable electric-indent-mode
 (add-hook 'js2-mode-hook
