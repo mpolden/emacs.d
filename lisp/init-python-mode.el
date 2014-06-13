bf4e07182db802511a0bcbfa34402825b14a7fee
Martin Polden <martin.polden@gmail.com>
2014-06-13 11:24:47 +0200

    Highlight lines exceeding 79 chars in python-mode

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,7 +1,10 @@
 (require 'python)
 
-;; disable electric indent
 (add-hook 'python-mode-hook
-          (lambda () (set (make-local-variable 'electric-indent-mode) nil)))
+          (lambda ()
+            ;; disable eletric-indent
+            (set (make-local-variable 'electric-indent-mode) nil)
+            ;; highlight lines longer than 79 characters (pep8)
+            (set (make-local-variable 'whitespace-line-column) 79)))
 
 (provide 'init-python-mode)
