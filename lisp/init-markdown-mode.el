41d802edf9b6ee45eb0f025857c520cebfd1da67
Martin Polden <martin.polden@gmail.com>
2014-06-13 15:28:03 +0200

    Fix file patterns for markdown-mode

--- lisp/init-markdown-mode.el
+++ lisp/init-markdown-mode.el
@@ -3,6 +3,6 @@
 
 ;; use markdown mode for .md and .markdown extensions
 (require 'markdown-mode)
-(add-to-list 'auto-mode-alist '("\\.(md|markdown)$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(md\\|markdown\\)\\'" . markdown-mode))
 
 (provide 'init-markdown-mode)
