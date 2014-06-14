0692acf6261202dcc751d3a6ad27cf97fbdeef13
Martin Polden <martin.polden@gmail.com>
2014-06-14 13:19:33 +0200

    Add missing quotes

--- lisp/init-ibuffer.el
+++ lisp/init-ibuffer.el
@@ -12,9 +12,9 @@
             (ibuffer-vc-set-filter-groups-by-vc-root)
             ;; make C-o and o behave as in dired
             (define-key ibuffer-mode-map (kbd "o")
-              ibuffer-visit-buffer-other-window)
+              'ibuffer-visit-buffer-other-window)
             (define-key ibuffer-mode-map (kbd "C-o")
-              ibuffer-visit-buffer-other-window-noselect)))
+              'ibuffer-visit-buffer-other-window-noselect)))
 
 ;; use ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer-other-window)
