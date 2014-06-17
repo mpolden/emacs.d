ddb63a91e2aa1a85e09cfd049fdaf4399c9f35a8
Martin Polden <martin.polden@gmail.com>
2014-06-17 18:29:48 +0200

    Use yaml-mode for Ansible group_vars and host_vars

--- lisp/init-yaml-mode.el
+++ lisp/init-yaml-mode.el
@@ -8,4 +8,7 @@
 (add-hook 'yaml-mode-hook
           (lambda () (set (make-local-variable 'electric-indent-mode) nil)))
 
+;; ansible group_vars and host_vars
+(add-to-list 'auto-mode-alist '("/\\(group\\|host\\)_vars/" . yaml-mode))
+
 (provide 'init-yaml-mode)
