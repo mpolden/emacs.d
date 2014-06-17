ccf7ca685fa76e795e8225e446da30d632025d2e
Martin Polden <martin.polden@gmail.com>
2014-06-17 19:37:37 +0200

    Prefer jq for reformatting JSON

--- lisp/init-js2-mode.el
+++ lisp/init-js2-mode.el
@@ -10,4 +10,22 @@
 (require 'json-mode)
 (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
 
+;; prefer jq for reformatting json
+(defun jq-reformat-region (begin end)
+  (interactive "r")
+  (if (executable-find "jq")
+      (shell-command-on-region
+       begin end "jq --monochrome-output --ascii-output ." (current-buffer) t)
+    (json-reformat-region begin end)))
+
+(defun jq-reformat ()
+  (interactive)
+  (if (use-region-p)
+      (jq-reformat-region (region-beginning) (region-end))
+    (jq-reformat-region (point-min) (point-max))))
+
+(add-hook 'json-mode-hook
+          (lambda ()
+            (define-key json-mode-map (kbd "C-c C-f") 'jq-reformat)))
+
 (provide 'init-js2-mode)
