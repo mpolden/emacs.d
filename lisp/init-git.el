2d0ba6ba4f121e8c9a42ddec7dbaec12c8a02663
Martin Polden <martin.polden@gmail.com>
2014-06-23 18:41:38 +0200

    Use dired-style keybindings in magit-status-mode

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -18,4 +18,19 @@
 ;; magit keybinding
 (global-set-key (kbd "C-x m") 'magit-status)
 
+(defun magit-visit-item-noselect (&optional other-window)
+  "Visit current item, but don't select it."
+  (interactive "P")
+  (let ((current-window (selected-window)))
+    (magit-visit-item other-window)
+    (select-window current-window)))
+
+(add-hook 'magit-status-mode-hook
+          (lambda ()
+            ;; make C-o and o behave as in dired
+            (define-key magit-status-mode-map (kbd "C-o")
+              'magit-visit-item-noselect)
+            (define-key magit-status-mode-map (kbd "o")
+              'magit-visit-item)))
+
 (provide 'init-git)
