8a3c8269060c2a3d7ee80ac34c713d65fbc3ff2c
Martin Polden <martin.polden@gmail.com>
2014-06-23 11:33:27 +0200

    Revert "Add clj-refactor"
    This reverts commit 8a8cc99f745a711495fbf6d90d78f0e9f3bde705.
    
    Conflicts:
    	lisp/init-lisp.el

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,19 +1,11 @@
 ;; install packages
 (require-package 'clojure-mode)
 (require-package 'cider)
-(require-package 'clj-refactor)
 (require-package 'paredit)
 
 (require 'clojure-mode)
 (require 'cider)
-(require 'clj-refactor)
 (require 'paredit)
-
-;; clj-refactor-mode keybinding
-(cljr-add-keybindings-with-prefix "C-c C-m")
-
-;; enable clj-refactor-mode
-(add-hook 'clojure-mode-hook (lambda () (clj-refactor-mode 1)))
 
 ;; prevent cider from prompting to save file
 (setq cider-prompt-save-file-on-load nil)
