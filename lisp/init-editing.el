460fde72af708cd6842fcb10a0be944264833ad4
Martin Polden <martin.polden@gmail.com>
2014-06-24 17:27:52 +0200

    Add rename-file-and-buffer and keybinding
    Stolen from Steve Yegges .emacs

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -120,4 +120,21 @@ Including indent-buffer, which should not be called automatically on save."
 ;; imenu (will use ido completion if ido-ubiquitous-mode is enabled)
 (global-set-key (kbd "C-c i") 'imenu)
 
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
+(global-set-key (kbd "C-c n") 'rename-file-and-buffer)
+
 (provide 'init-editing)
