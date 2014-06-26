20cdb944c3fadfad6bd6945d553a8116af124ae5
Martin Polden <martin.polden@gmail.com>
2014-06-26 15:33:25 +0200

    Use flat index in imenu

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -5,6 +5,9 @@
             ;; disable eletric-indent
             (set (make-local-variable 'electric-indent-mode) nil)
             ;; highlight lines longer than 79 characters (pep8)
-            (set (make-local-variable 'whitespace-line-column) 79)))
+            (set (make-local-variable 'whitespace-line-column) 79)
+            ;; use flat index in imenu
+            (set (make-local-variable 'imenu-create-index-function)
+                 'python-imenu-create-flat-index)))
 
 (provide 'init-python-mode)
