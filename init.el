f7f0364f78f2b25534c07870c78453577dcb15f0
Martin Polden <martin.polden@gmail.com>
2014-06-26 15:06:00 +0200

    Use ~/.emacs.d/site-lisp for backported code

--- init.el
+++ init.el
@@ -1,6 +1,12 @@
 ;; add ~/.emacs.d/lisp to load path
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
+;; add ~/.emacs.d/site-lisp to load path, if it exists
+;; used for backported code
+(let ((site-lisp (expand-file-name "site-lisp" user-emacs-directory)))
+  (when (file-directory-p site-lisp)
+    (add-to-list 'load-path site-lisp)))
+
 ;; load files
 (require 'init-package)
 (require 'init-appearance)
