4ec089e19a07fe155cddf07d7ca91adb7fe321a2
Martin Polden <martin.polden@gmail.com>
2014-06-28 16:13:53 +0200

    Add keybinding for running gofmt

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -1,6 +1,11 @@
 ;; install package
 (require-package 'go-mode)
 
-(require 'go-mode-load)
+(require 'go-mode)
+
+(add-hook 'go-mode-hook
+          (lambda ()
+            ;; C-c p runs gofmt on the buffer
+            (define-key go-mode-map (kbd "C-c p") 'gofmt)))
 
 (provide 'init-go-mode)
