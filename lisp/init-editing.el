ac5d3c35f7eb9efe85d91d9bee7a65f0415cd608
Martin Polden <martin.polden@gmail.com>
2014-07-04 23:14:17 +0200

    Add eshell-other-window

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -136,4 +136,17 @@ Including indent-buffer, which should not be called automatically on save."
 
 (global-set-key (kbd "C-c n") 'rename-file-and-buffer)
 
+(defun eshell-other-window (&optional noselect)
+  "Create an interactive Eshell buffer in another window."
+  (interactive)
+  (with-current-buffer (get-buffer-create eshell-buffer-name)
+    (unless (eq major-mode 'eshell-mode)
+      (eshell-mode)))
+  (let ((current-window (selected-window)))
+    (switch-to-buffer-other-window eshell-buffer-name)
+    (when noselect
+      (select-window current-window))))
+
+(global-set-key (kbd "C-c k") 'eshell-other-window)
+
 (provide 'init-editing)
