d6f6560cd1a9562d9c2f03138dbc6f02ca8853eb
Martin Polden <martin.polden@gmail.com>
2014-07-05 22:18:26 +0200

    Enable flyspell in markdown-mode

--- lisp/init-markdown-mode.el
+++ lisp/init-markdown-mode.el
@@ -5,4 +5,11 @@
 (require 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.\\(md\\|markdown\\)\\'" . markdown-mode))
 
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            ;; disable electric indent
+            (set (make-local-variable 'electric-indent-mode) nil)
+            ;; enable flyspell
+            (flyspell-mode 1)))
+
 (provide 'init-markdown-mode)
