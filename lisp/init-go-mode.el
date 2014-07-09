9a670c52417815e22200242c626e0622803fb5ee
Martin Polden <martin.polden@gmail.com>
2014-07-09 19:00:16 +0200

    Use flat imenu index in go-mode

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -6,6 +6,10 @@
 (add-hook 'go-mode-hook
           (lambda ()
             ;; C-c p runs gofmt on the buffer
-            (define-key go-mode-map (kbd "C-c p") 'gofmt)))
+            (define-key go-mode-map (kbd "C-c p") 'gofmt)
+            ;; use flat index for imenu
+            (setq-local imenu-generic-expression
+                        (mapcar (lambda (arg) (cons nil (cdr arg)))
+                                imenu-generic-expression))))
 
 (provide 'init-go-mode)
