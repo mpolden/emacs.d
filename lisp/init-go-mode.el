d64d81cb2610dc768eb8584bc24510a9246b5a61
Martin Polden <martin.polden@gmail.com>
2014-07-25 02:15:03 +0200

    Revert "Only set imenu-generic-expression if already set"
    This reverts commit 109757bbc9c14013b7a42040496475553b79bf94.

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -8,9 +8,8 @@
             ;; C-c p runs gofmt on the buffer
             (define-key go-mode-map (kbd "C-c p") 'gofmt)
             ;; use flat index for imenu
-            (when (local-variable-p 'imenu-generic-expression)
-              (setq-local imenu-generic-expression
-                          (mapcar (lambda (arg) (cons nil (cdr arg)))
-                                  imenu-generic-expression)))))
+            (setq-local imenu-generic-expression
+                        (mapcar (lambda (arg) (cons nil (cdr arg)))
+                                imenu-generic-expression))))
 
 (provide 'init-go-mode)
