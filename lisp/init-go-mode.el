74ebee8e32a7261a0e89bb0964c03c959207a3d6
Martin Polden <martin.polden@gmail.com>
2014-08-24 13:45:16 +0200

    Add function for setting GOPATH

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -2,6 +2,7 @@
 (require-package 'go-mode)
 
 (require 'go-mode)
+(require 'vc)
 
 (defun go-mode-create-imenu-index ()
   "Create and return an imenu index alist. Unlike the default
@@ -27,6 +28,17 @@ items follow a style that is consistent with other prog-modes."
 (defun go-mode-create-flat-imenu-index ()
   "Return a flat imenu index alist. See `go-mode-create-imenu-index'."
   (apply 'nconc (mapcar 'cdr (go-mode-create-imenu-index))))
+
+(defun go-mode-set-gopath ()
+  "Set GOPATH using the VCS root of the current buffer."
+  (interactive)
+  (let ((repo-root (if buffer-file-name (or (vc-git-root buffer-file-name)
+                                            (vc-hg-root buffer-file-name))))
+        (gopath (getenv "GOPATH")))
+    (when repo-root
+      (unless (equal gopath repo-root)
+        (setenv "GOPATH" (expand-file-name repo-root))
+        (message "GOPATH=%s" (getenv "GOPATH"))))))
 
 (add-hook 'go-mode-hook
           (lambda ()
