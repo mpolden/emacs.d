f47798f1bce74204f031345cb7d001668a2e1667
Martin Polden <martin.polden@gmail.com>
2014-11-14 10:00:36 +0100

    Remove unused shell functions

--- init.el
+++ init.el
@@ -24,7 +24,6 @@
 (require 'init-expand-region)
 (require 'init-python-mode)
 (require 'init-web-mode)
-(require 'init-shell)
 (require 'init-imenu)
 (require 'init-dockerfile)
 (require 'init-tramp)
