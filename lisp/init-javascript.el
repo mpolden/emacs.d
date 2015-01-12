e44b6f6c8b2cf4f873f395ed0005698a357b0e6e
Martin Polden <martin.polden@gmail.com>
2015-01-12 22:33:51 +0100

    Remove js2-mode

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -1,48 +1,8 @@
-;; install package
-(require-package 'js2-mode)
-
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-
-;; set indent level to 2
-(setq-default js2-basic-offset 2)
-(setq-default js-indent-level 2)
-
-;; highlight built-in functions, such as filter, forEach and map
-(setq js2-highlight-level 3)
-
-;; don't override M-j
-(eval-after-load "js2-mode"
-  '(define-key js2-mode-map (kbd "M-j") nil))
-
-;; js2-mode can't handle json
 (require 'js)
 (add-to-list 'auto-mode-alist '("\\.\\(json\\|jshintrc\\)\\'" . js-mode))
 
-(defun js2-imenu-create-flat-index (&optional alist prefix)
-  "Return a flattened alist for `imenu--index-alist'."
-  ;; adapted from python-imenu-create-flat-index in python.el
-  (apply
-   'nconc
-   (mapcar
-    (lambda (item)
-      (let ((name (if prefix
-                      (concat prefix "." (car item))
-                    (car item)))
-            (pos (cdr item)))
-        (cond ((or (numberp pos) (markerp pos))
-               (list (cons name pos)))
-              ((listp pos)
-               (js2-imenu-create-flat-index pos name)))))
-    (or alist (js2-mode-create-imenu-index)))))
-
-;; enable imenu extras
-(require 'js2-imenu-extras)
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (js2-imenu-extras-mode 1)
-            (setq-local imenu-create-index-function
-                        #'js2-imenu-create-flat-index)))
+;; set indent level to 2
+(setq-default js-indent-level 2)
 
 ;; use jq for reformatting json
 (defun jq-reformat-region (begin end)
