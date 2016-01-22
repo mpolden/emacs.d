87dcd5727b2e70f79f4ff20f37bf5b2b9174e21d
Martin Polden <martin.polden@gmail.com>
2016-01-22 23:27:36 +0100

    Remove unused nosetests functions

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,30 +1,4 @@
 (require 'python)
-(require 'magit)
-
-(defun nosetests-command-at-point ()
-  "Create a nosetests command with the test at point as the argument."
-  (if (eq 'python-mode major-mode)
-      (let* ((git-root (magit-toplevel))
-             (file-rel-name (when git-root (file-relative-name
-                                            buffer-file-name git-root))))
-        (if file-rel-name
-            (let ((function (which-function)))
-              (if function
-                  (concat "nosetests " file-rel-name ":" function)
-                (message "Could not determine function at point.")
-                nil))
-          (message "Buffer is not visiting a file, or file is not in git.")
-          nil))
-    (message "Not in python-mode.")
-    nil))
-
-(defun kill-nosetests-command-at-point ()
-  "Kill nosetests command at point."
-  (interactive)
-  (let ((command (nosetests-command-at-point)))
-    (when command
-      (kill-new command)
-      (message "Killed nosetests command."))))
 
 (add-hook 'python-mode-hook
           (lambda ()
@@ -34,9 +8,6 @@
             (setq-local fill-column 79)
             ;; use flat index in imenu
             (setq-local imenu-create-index-function
-                        'python-imenu-create-flat-index)
-            ;; add nosetest command to kill ring
-            (define-key python-mode-map (kbd "C-c t")
-              'kill-nosetests-command-at-point)))
+                        'python-imenu-create-flat-index)))
 
 (provide 'init-python-mode)
