fbbe4d2acfc0d2639bcaec25fde0e3321b6a7149
Martin Polden <martin.polden@gmail.com>
2016-02-25 20:18:15 +0100

    Remove clojure-mode

--- lisp/init-lisp.el
+++ lisp/init-lisp.el
@@ -1,11 +1,8 @@
-;; install packages
-(require-packages '(clojure-mode paredit))
+(require-package 'paredit)
 
-(require 'clojure-mode)
 (require 'paredit)
 
 ;; enable paredit
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-(add-hook 'clojure-mode-hook 'paredit-mode)
 
 (provide 'init-lisp)
