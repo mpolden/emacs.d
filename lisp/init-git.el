1f94ebf06c7818239feffd4d799d7eb77f271dfd
Martin Polden <martin.polden@gmail.com>
2016-02-28 21:03:37 +0100

    Fix o and C-o behaviour in magit-status

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -20,15 +20,23 @@
          ("C-c b" . magit-blame)
          :map magit-status-mode-map
          ;; make C-o and o behave as in dired
-         ("o" . magit-diff-visit-file)
-         ("C-o" . magit-diff-visit-file-noselect))
+         ("o" . magit-diff-visit-file-other-window)
+         ("C-o" . magit-diff-visit-file-other-window-noselect))
 
   :config
-  (defun magit-diff-visit-file-noselect ()
-    "Visit current item, but don't select it."
+  (defun magit-diff-visit-file-other-window (&optional noselect)
+    "Visit current file in another window."
     (interactive)
-    (let ((current-window (selected-window)))
+    (let ((current-window (selected-window))
+          ;; magit-diff-visit-file visits in other-window with prefix arg
+          (current-prefix-arg t))
       (call-interactively 'magit-diff-visit-file)
-      (select-window current-window))))
+      (when noselect
+        (select-window current-window))))
+
+  (defun magit-diff-visit-file-other-window-noselect ()
+    "Visit current file in another window, but don't select it."
+    (interactive)
+    (magit-diff-visit-file-other-window t)))
 
 (provide 'init-git)
