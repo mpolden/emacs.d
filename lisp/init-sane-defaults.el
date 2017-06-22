33d327cfd9192e00a9cb4175da1ea1cc108dcd37
Martin Polden <mpolden@mpolden.no>
2017-06-22 18:01:16 +0200

    Group directories first in dired

--- lisp/init-sane-defaults.el
+++ lisp/init-sane-defaults.el
@@ -58,6 +58,12 @@
 ;; show human readable sizes in dired
 (setq dired-listing-switches "-alh")
 
+;; group directories first in dired if supported
+(when (eq 0 (call-process insert-directory-program
+                          nil nil nil "--group-directories-first"))
+  (setq dired-listing-switches (concat dired-listing-switches
+                                       " --group-directories-first")))
+
 ;; single space ends a sentence
 (setq-default sentence-end-double-space nil)
 
