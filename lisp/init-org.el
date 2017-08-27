378c77485db7e49129e03515992b81ecc1872215
Martin Polden <mpolden@mpolden.no>
2017-08-27 16:15:55 +0200

    Configure MobileOrg integration

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,6 +1,9 @@
 (use-package org
   :ensure nil ;; package is bundled with emacs
   :init
-  (setq org-archive-location "::* Archived Tasks"))
+  (setq org-archive-location "::* Archived Tasks"
+        org-agenda-files '("todo.org")
+        org-refile-targets '((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5))
+        org-mobile-directory "~/.mobileorg"))
 
 (provide 'init-org)
