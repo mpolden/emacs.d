aae4704d557268d85bf65fa278d3e9a3480fd70b
Martin Polden <mpolden@mpolden.no>
2017-10-04 19:30:52 +0200

    Show human readable free space in dired

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -4,6 +4,9 @@
   ;; show human readable sizes in dired
   (setq dired-listing-switches "-alh")
 
+  ;; show human readable free space
+  (setq directory-free-space-args "-h")
+
   ;; group directories first in dired if supported
   (when (eq 0 (call-process insert-directory-program
                             nil nil nil "--group-directories-first"))
