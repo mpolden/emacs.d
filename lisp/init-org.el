9f6ca740397982b2eab919fd59426984857bc7e7
Martin Polden <mpolden@mpolden.no>
2017-10-05 19:43:38 +0200

    Fix warning

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1,9 +1,9 @@
 (use-package org
   :ensure nil ;; package is bundled with emacs
   :bind ("C-c a" . org-agenda)
-  :init
+  :config
   (setq org-archive-location "::* Archived Tasks"
-        org-agenda-files `(,org-directory)
+        org-agenda-files (list org-directory)
         org-refile-targets '((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5))
         org-mobile-directory "~/.mobileorg"
         org-log-done 'time))
