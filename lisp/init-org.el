9c261990a738ecca90dac56f8db352040a256cd2
Martin Polden <mpolden@mpolden.no>
2017-10-11 21:06:23 +0200

    Comment org-mode configuration

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,10 +2,20 @@
   :ensure nil ;; package is bundled with emacs
   :bind ("C-c a" . org-agenda)
   :config
-  (setq org-archive-location "::* Archived Tasks"
-        org-agenda-files (list org-directory)
-        org-refile-targets '((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5))
-        org-mobile-directory "~/.mobileorg"
-        org-log-done 'time))
+  ;; name of subtree where archived tasks should be moved
+  (setq org-archive-location "::* Archived Tasks")
+
+  ;; display all org files in agenda
+  (setq org-agenda-files (list org-directory))
+
+  ;; refile targets 1 level in current buffer and all org agenda files
+  (setq org-refile-targets '((nil :maxlevel . 1) (org-agenda-files :maxlevel . 1)))
+
+  ;; path used by org-mobile-push and org-mobile-pull
+  ;; should be the path that mobileorg uploads to, e.g. a mounted webdav share
+  (setq org-mobile-directory "~/.mobileorg")
+
+  ;; record time when moving a task to done state
+  (setq org-log-done 'time))
 
 (provide 'init-org)
