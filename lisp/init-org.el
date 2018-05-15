944da46be8586d9e0247572ba362fdd83e613a3f
Martin Polden <mpolden@mpolden.no>
2018-05-15 14:48:47 +0200

    Remove unused org-mobile setting

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -17,10 +17,6 @@
   ;; refile targets 1 level in current buffer and all org agenda files
   (setq org-refile-targets '((nil :maxlevel . 1) (org-agenda-files :maxlevel . 1)))
 
-  ;; path used by org-mobile-push and org-mobile-pull
-  ;; should be the path that mobileorg uploads to, e.g. a mounted webdav share
-  (setq org-mobile-directory "~/.mobileorg")
-
   ;; record time when moving a task to done state
   (setq org-log-done 'time)
 
