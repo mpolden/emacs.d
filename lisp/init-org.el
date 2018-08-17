82cc4ecd5b33f5e1892b759d2086131b66222eb6
Martin Polden <mpolden@mpolden.no>
2018-08-17 09:36:53 +0200

    Use inactive timestamps in capture template

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -26,12 +26,12 @@
 
   ;; capture template including title, date and time
   (setq org-capture-templates '(("p" "Personal" entry (file+headline "" "Tasks")
-                                 "* TODO %?\n  %T"
+                                 "* TODO %?\n  %U"
                                  :prepend t
                                  :empty-lines-after 1)
                                 ("w" "Work" entry (file+headline "work.org"
                                                                  "Tasks")
-                                 "* TODO %?\n  %T"
+                                 "* TODO %?\n  %U"
                                  :prepend t
                                  :empty-lines-after 1)))
 
