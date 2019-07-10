402e4eac79c7a21d0a38190c4c560b9fa34192df
Martin Polden <mpolden@mpolden.no>
2019-07-10 18:08:53 +0200

    Clarify use of prefix arg

--- lisp/init-compile.el
+++ lisp/init-compile.el
@@ -5,6 +5,7 @@
   ;; avoid reading command when compiling
   (setq compilation-read-command nil)
 
+  ;; C-u C-c m will always read command
   :bind (("C-c m" . 'compile)))
 
 (provide 'init-compile)
