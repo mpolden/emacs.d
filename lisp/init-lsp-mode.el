e3ea70589c01c670423a3abc67b09f45623981d8
Martin Polden <mpolden@mpolden.no>
2019-07-10 19:58:13 +0200

    Remove lsp-ui-mode

--- lisp/init-lsp-mode.el
+++ lisp/init-lsp-mode.el
@@ -4,18 +4,12 @@
   ;; disable yasnippet support
   (setq lsp-enable-snippet nil)
   ;; prefer lsp-ui
-  (setq lsp-prefer-flymake nil)
+  (setq lsp-prefer-flymake :none)
   :hook
   ;; enable lsp in go-mode
   (go-mode . lsp)
   :bind (:map lsp-mode-map
               ;; C-c r renames identifier
               ("C-c r" . lsp-rename)))
-
-(use-package lsp-ui
-  :commands lsp-ui-mode
-  :hook
-  ;; enable lsp-ui-mode in lsp-mode
-  (lsp . lsp-ui-mode))
 
 (provide 'init-lsp-mode)
