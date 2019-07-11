f6868dac674a8c2520027fcea8ca279e7d3a7b6e
Martin Polden <mpolden@mpolden.no>
2019-07-11 22:54:38 +0200

    Remove imenu customization for go-mode
    lsp-mode now provides a sane imenu.

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -1,28 +1,3 @@
-(defun go-mode-create-imenu-index ()
-    "Create and return an imenu index alist. Unlike the default
-alist created by go-mode, this method creates an alist where
-items follow a style that is consistent with other prog-modes."
-    (let* ((patterns '(("type" "^type *\\([^ \t\n\r\f]*\\)" 1)))
-           (type-index (imenu--generic-function patterns))
-           (func-index))
-      (save-excursion
-        (goto-char (point-min))
-        (while (re-search-forward go-func-meth-regexp (point-max) t)
-          (let* ((var (match-string-no-properties 1))
-                 (func (match-string-no-properties 2))
-                 (name (if var
-                           (concat (substring var 0 -1) "." func)
-                         func))
-                 (beg (match-beginning 0))
-                 (marker (copy-marker beg))
-                 (item (cons name marker)))
-            (setq func-index (cons item func-index)))))
-      (nconc type-index (list (cons "func" func-index)))))
-
-(defun go-mode-create-flat-imenu-index ()
-    "Return a flat imenu index alist. See `go-mode-create-imenu-index'."
-    (apply 'nconc (mapcar 'cdr (go-mode-create-imenu-index))))
-
 (use-package go-mode
   :init
   ;; use goimports if available
@@ -39,9 +14,6 @@ items follow a style that is consistent with other prog-modes."
   (add-hook 'go-mode-hook
             (lambda ()
               ;; adjust fill-column
-              (setq-local fill-column 120)
-              ;; use flat imenu index
-              (setq-local imenu-create-index-function
-                          #'go-mode-create-flat-imenu-index))))
+              (setq-local fill-column 120))))
 
 (provide 'init-go-mode)
