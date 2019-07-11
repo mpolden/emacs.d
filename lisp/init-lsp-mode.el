01a80db2e33784146632316160513290eaf18eb0
Martin Polden <mpolden@mpolden.no>
2019-07-11 22:43:32 +0200

    Revert "Remove lsp-ui-mode"
    This reverts commit e3ea70589c01c670423a3abc67b09f45623981d8.

--- lisp/init-lsp-mode.el
+++ lisp/init-lsp-mode.el
@@ -4,12 +4,18 @@
   ;; disable yasnippet support
   (setq lsp-enable-snippet nil)
   ;; prefer lsp-ui
-  (setq lsp-prefer-flymake :none)
+  (setq lsp-prefer-flymake nil)
   :hook
   ;; enable lsp in go-mode
   (go-mode . lsp)
   :bind (:map lsp-mode-map
               ;; C-c r renames identifier
               ("C-c r" . lsp-rename)))
+
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :hook
+  ;; enable lsp-ui-mode in lsp-mode
+  (lsp . lsp-ui-mode))
 
 (provide 'init-lsp-mode)
