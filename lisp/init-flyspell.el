c09120f9c9c030b90ddcfb74db51902f5827adcd
Martin Polden <mpolden@mpolden.no>
2019-07-16 11:18:02 +0200

    Load flyspell-mode for latex-mode

--- lisp/init-flyspell.el
+++ lisp/init-flyspell.el
@@ -5,7 +5,8 @@
   ;; load flyspell automatically for these modes
   :hook ((git-commit-mode . flyspell-mode)
          (gfm-mode . flyspell-mode)
-         (org-mode . flyspell-mode))
+         (org-mode . flyspell-mode)
+         (latex-mode . flyspell-mode))
 
   :bind (:map flyspell-mode-map
               ("C-c s" . flyspell-correct-word-before-point)))
