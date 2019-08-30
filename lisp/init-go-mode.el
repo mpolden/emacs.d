062fbada097ed505aa786e75ab77a767a4d1211a
Martin Polden <mpolden@mpolden.no>
2019-08-30 21:25:45 +0200

    Let lsp handle gofmt

--- lisp/init-go-mode.el
+++ lisp/init-go-mode.el
@@ -1,16 +1,6 @@
 (use-package go-mode
   :init
-  ;; use goimports if available
-  (when (executable-find "goimports")
-    (setq gofmt-command "goimports"))
-
-  :bind (:map go-mode-map
-         ;; C-c p runs gofmt on the buffer
-         ("C-c p" . gofmt))
-
   :config
-  ;; run gofmt before saving file
-  (add-hook 'before-save-hook 'gofmt-before-save)
   (add-hook 'go-mode-hook
             (lambda ()
               ;; adjust fill-column
--- lisp/init-lsp-mode.el
+++ lisp/init-lsp-mode.el
@@ -1,5 +1,5 @@
 (use-package lsp-mode
-  :commands lsp
+  :commands (lsp lsp-format-buffer)
   :init
   ;; disable yasnippet support
   (setq lsp-enable-snippet nil)
@@ -7,10 +7,14 @@
   (setq lsp-prefer-flymake nil)
   :hook
   ;; enable lsp in go-mode
-  (go-mode . lsp)
+  ((go-mode . lsp)
+   ;; reformat on save
+   (before-save . lsp-format-buffer))
   :bind (:map lsp-mode-map
               ;; C-c r renames identifier
-              ("C-c r" . lsp-rename)))
+              ("C-c r" . lsp-rename)
+              ;; C-c p reformats buffer
+              ("C-c p" . lsp-format-buffer)))
 
 (use-package lsp-ui
   :commands lsp-ui-mode
