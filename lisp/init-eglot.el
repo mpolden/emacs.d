c9d8509c5bf2f1aee7d4859c2234a683d0a4cb9a
Martin Polden <mpolden@mpolden.no>
2020-01-26 13:19:19 +0100

    Add comment with gopls instructions

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,6 +1,7 @@
 (use-package eglot
   :hook
   ;; enable eglot in go-mode
+  ;; requires gopls: env GO111MODULE=on go get golang.org/x/tools/gopls@latest
   ((go-mode . eglot-ensure)
    ;; format on save
    (before-save . eglot-format))
@@ -10,7 +11,7 @@
               ;; C-c p formats buffer
               ("C-c p" . eglot-format))
   :config
-  ;; disable imenu integration as it's currently unsupported
+  ;; disable imenu integration as it's currently unsupported with gopls
   ;; https://github.com/joaotavora/eglot/pull/303
   (setq eglot-stay-out-of '("imenu")))
 
