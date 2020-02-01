cc1df96fc468d16d6d8b7688b1581a0ddc77fcc1
Martin Polden <mpolden@mpolden.no>
2020-02-01 22:22:48 +0100

    Leave electric-indent-mode enabled in python-mode

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -7,8 +7,6 @@
   ;; set buffer local variables
   (add-hook 'python-mode-hook
           (lambda ()
-            ;; disable electric indent
-            (setq-local electric-indent-mode nil)
             ;; highlight lines longer than 88 characters
             (setq-local fill-column 88)
             ;; use flat index in imenu
