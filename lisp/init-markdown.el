4011af4d089807b1ba196f0a8eeee2c15ba6e730
Martin Polden <mpolden@mpolden.no>
2020-02-01 23:23:23 +0100

    Leave electric-indent-mode enabled in markdown-mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -5,12 +5,6 @@
   (("\\.\\(md\\|markdown\\)\\'" . gfm-mode)
    ;; use gfm mode for pull request and issue buffers
    ("PULLREQ_EDITMSG" . gfm-mode)
-   ("ISSUE_EDITMSG" . gfm-mode))
-
-  :config
-  (add-hook 'markdown-mode-hook
-          (lambda ()
-            ;; disable electric indent
-            (setq-local electric-indent-mode nil))))
+   ("ISSUE_EDITMSG" . gfm-mode)))
 
 (provide 'init-markdown)
