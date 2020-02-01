745270ad3b794c0f03123ec87c481fa779a9267c
Martin Polden <mpolden@mpolden.no>
2020-02-01 22:33:19 +0100

    Leave electric-indent-mode enabled in yaml-mode

--- lisp/init-yaml.el
+++ lisp/init-yaml.el
@@ -1,11 +1,6 @@
 (use-package yaml-mode
   :mode
   ;; ansible group_vars and host_vars
-  ("/\\(group\\|host\\)_vars/" . yaml-mode)
-
-  :config
-  ;; disable electric indent
-  (add-hook 'yaml-mode-hook
-            (lambda () (setq-local electric-indent-mode nil))))
+  ("/\\(group\\|host\\)_vars/" . yaml-mode))
 
 (provide 'init-yaml)
