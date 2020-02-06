57df8519fdfbaa8cc814153393bf8ed4abca551d
Martin Polden <mpolden@mpolden.no>
2020-02-06 22:31:52 +0100

    Move dynamic configuration to :config
    Since this is calling an external process, it's faster to defer it until the
    package is loaded.

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -7,11 +7,13 @@
   ;; show human readable free space
   (setq directory-free-space-args "-h")
 
+  :bind ("M-<up>" . dired-up-directory)
+
+  :config
   ;; group directories first in dired if supported
   (when (eq 0 (call-process insert-directory-program
                             nil nil nil "--group-directories-first"))
     (setq dired-listing-switches (concat dired-listing-switches
-                                         " --group-directories-first")))
-  :bind ("M-<up>" . dired-up-directory))
+                                         " --group-directories-first"))))
 
 (provide 'init-dired)
