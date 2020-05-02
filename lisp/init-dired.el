59fc0cfe9885b43e5b989ca4e5d9e6a90783f465
Martin Polden <mpolden@mpolden.no>
2020-05-02 11:46:25 +0200

    C-c C-o opens file in dired

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,3 +1,11 @@
+(defun dired-open-or-shell-command ()
+  (interactive)
+  (if (eq system-type 'darwin)
+      ;; call open directly on darwin
+      (dired-do-shell-command "open" nil (dired-get-marked-files t))
+    ;; otherwise prompt for shell command
+    (call-interactively 'dired-do-shell-command)))
+
 (use-package dired
   :ensure nil ;; package is bundled with emacs
   :init
@@ -7,7 +15,8 @@
   ;; show human readable free space
   (setq directory-free-space-args "-h")
 
-  :bind ("M-<up>" . dired-up-directory)
+  :bind (("M-<up>" . dired-up-directory)
+         ("C-c C-o" . dired-open-or-shell-command))
 
   :config
   ;; group directories first in dired if supported
