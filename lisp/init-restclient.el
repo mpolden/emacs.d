6f82a5376ed79ddc76f7f3d5841d4fc67e9623a8
Martin Polden <mpolden@mpolden.no>
2020-09-23 13:23:32 +0200

    restclient: Load automatically for http extension

--- lisp/init-restclient.el
+++ lisp/init-restclient.el
@@ -4,6 +4,8 @@
   ;; limit number of times vars are substituted
   (setq restclient-vars-max-passes 2)
   ;; use matching client certificates from ~/.authinfo
-  (setq network-stream-use-client-certificates t))
+  (setq network-stream-use-client-certificates t)
+  ;; use this mode for .http extension
+  :mode (("\\.http\\'" . restclient-mode)))
 
 (provide 'init-restclient)
