0ad4c9e696cf59f7ec61b877c4b3f1b117f9ef6c
Martin Polden <mpolden@mpolden.no>
2020-10-28 20:41:20 +0100

    ivy: Remove unused swiper config

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -4,18 +4,12 @@
 
 (use-package ivy
   :ensure t
-  :demand t ;; this cannot lazy load as bindings are inside a mode map
   :after flx
   :diminish ivy-mode
   :init
-  (setq ivy-re-builders-alist
-        ;; use regular matching in swiper
-        '((swiper . ivy--regex-plus)
-          ;; use fuzzy matching by default'
-          (t . ivy--regex-fuzzy)))
-  :bind (:map ivy-minibuffer-map
-              ;; C-r selects previous candidate, like isearch
-              ("C-r" . ivy-previous-line-or-history))
+  ;; use fuzzy matching by default'
+  (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+
   :config
   (ivy-mode 1))
 
