9d98b299f7527152449b8b1620b3f1231a9afe06
Martin Polden <mpolden@mpolden.no>
2020-11-01 10:46:10 +0100

    project: Read command when compiling

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -5,9 +5,6 @@
 (use-package project
   :ensure t
   :init
-  ;; avoid reading command when compiling
-  (setq compilation-read-command nil)
-
   ;; commands to show when switching projects
   (setq project-switch-commands '((?f "Find file" project-find-file)
                                   (?d "Dired" project-dired)
@@ -19,7 +16,6 @@
          ;; C-c p switches project
          ("C-c p" . project-switch-project)
          ;; C-c m compiles project
-         ;; C-u C-c m will force reading command
          ("C-c m" . project-compile)))
 
 (provide 'init-project)
