68041767495ec1f4e04e4e4948a531104d88347e
Martin Polden <mpolden@mpolden.no>
2020-12-28 11:37:46 +0100

    rust: Ensure eglot uses correct tab size

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,8 +1,15 @@
+(defun rust-mode-buffer-local-variables ()
+  ;; eglot passes tab-width as tab size when formatting code, when using rustfmt
+  ;; this should be 4
+  (setq-local tab-width 4))
+
 (use-package rust-mode
   :ensure t
   :init
   (when (executable-find "rustfmt")
     (setq rust-format-on-save t))
+
+  :hook (rust-mode . rust-mode-buffer-local-variables)
 
   :bind (:map rust-mode-map
               ;; C-c f runs rustfmt on the buffer
