21a99c6f9715e36281e55a4279f68d7cfbbb8e25
Martin Polden <mpolden@mpolden.no>
2020-12-28 10:24:23 +0100

    eglot: Enable in rust-mode

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -2,10 +2,12 @@
   :ensure t
   :hook
   ;; load eglot automatically for these modes
-  ;; go requires gopls: env GO111MODULE=on go get golang.org/x/tools/gopls@latest
-  ;; python requires pyls: python3 -m pip install --user python-language-server
+  ;; - go with gopls: env GO111MODULE=on go get -u golang.org/x/tools/gopls@latest
+  ;; - python with pyls: python3 -m pip install --user python-language-server
+  ;; - rust with rls: rustup component add rls
   ((go-mode . eglot-ensure)
    (python-mode . eglot-ensure)
+   (rust-mode . eglot-ensure)
    ;; format on save
    (before-save . eglot-format))
 
