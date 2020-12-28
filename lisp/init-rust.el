27d8c4f592bf79558c9cba91e967b2df80b6d42b
Martin Polden <mpolden@mpolden.no>
2020-12-28 11:42:09 +0100

    rust: Let eglot handle formatting

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -5,15 +5,8 @@
 
 (use-package rust-mode
   :ensure t
-  :init
-  (when (executable-find "rustfmt")
-    (setq rust-format-on-save t))
-
   :hook (rust-mode . rust-mode-buffer-local-variables)
-
   :bind (:map rust-mode-map
-              ;; C-c f runs rustfmt on the buffer
-              ("C-c f" . rust-format-buffer)
               ("C-c l" . rust-clippy)
               ("C-c t" . rust-test)
               ("C-c R" . rust-run)))
