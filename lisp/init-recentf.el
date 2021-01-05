5b959b3109b0ef2cfe0a721b1294d716c428ec9c
Martin Polden <mpolden@mpolden.no>
2021-01-05 08:50:04 +0100

    recentf: Update recentf-exclude after loading package

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,11 +1,9 @@
 (use-package recentf
-  :init
-  ;; ignore files in elpa directory as these may be opened by package.el
-  (add-to-list 'recentf-exclude (expand-file-name "elpa" user-emacs-directory))
-
   :bind (("C-x C-r" . recentf-open-files))
 
   :config
+  ;; ignore files in elpa directory as these may be opened by package.el
+  (add-to-list 'recentf-exclude (expand-file-name "elpa" user-emacs-directory))
   (recentf-mode 1))
 
 (provide 'init-recentf)
