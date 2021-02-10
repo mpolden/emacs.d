15694a07979d2bced0718e616820cf12c64aed22
Martin Polden <mpolden@mpolden.no>
2021-02-10 18:45:31 +0100

    recentf: Avoid cleaning up tramp buffers

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,4 +1,8 @@
 (use-package recentf
+  :init
+  ;; avoid cleaning up tramp buffers
+  (setq recentf-auto-cleanup 'never)
+
   :config
   ;; ignore files in elpa directory as these may be opened by package.el
   (add-to-list 'recentf-exclude (expand-file-name "elpa" user-emacs-directory))
