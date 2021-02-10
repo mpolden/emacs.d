7b7e615e3d3b4f56468c7a91bdecd1e7961148de
Martin Polden <mpolden@mpolden.no>
2021-02-10 19:16:52 +0100

    Revert "recentf: Avoid cleaning up tramp buffers"
    This reverts commit 15694a07979d2bced0718e616820cf12c64aed22.

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,8 +1,4 @@
 (use-package recentf
-  :init
-  ;; avoid cleaning up tramp buffers
-  (setq recentf-auto-cleanup 'never)
-
   :config
   ;; ignore files in elpa directory as these may be opened by package.el
   (add-to-list 'recentf-exclude (expand-file-name "elpa" user-emacs-directory))
