747860f0186805d2d45e782352dfc63f66424abc
Martin Polden <mpolden@mpolden.no>
2021-02-10 19:19:33 +0100

    recent: Set number of entries to save

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -1,4 +1,8 @@
 (use-package recentf
+  :init
+  ;; set number of entries to save
+  (setq recentf-max-saved-items 1000)
+
   :config
   ;; ignore files in elpa directory as these may be opened by package.el
   (add-to-list 'recentf-exclude (expand-file-name "elpa" user-emacs-directory))
