c5f8e1143af94ce7c0564e0b6379a885505962a7
Martin Polden <mpolden@mpolden.no>
2021-03-14 22:04:11 +0100

    rust: Stop setting tab-width

--- lisp/init-rust.el
+++ lisp/init-rust.el
@@ -1,11 +1,5 @@
-(defun rust-mode-buffer-local-variables ()
-  ;; eglot passes tab-width as tab size when formatting code, when using rustfmt
-  ;; this should be 4
-  (setq-local tab-width 4))
-
 (use-package rust-mode
   :ensure t
-  :hook (rust-mode . rust-mode-buffer-local-variables)
   :bind (:map rust-mode-map
               ("C-c l" . rust-clippy)
               ("C-c t" . rust-test)
