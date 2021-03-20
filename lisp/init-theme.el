191270886111a3b5947605545487789ffc12f439
Martin Polden <mpolden@mpolden.no>
2021-03-20 23:05:13 +0100

    theme: Let OS decide title bar styling

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -6,12 +6,7 @@
   :ensure t
   :if (display-graphic-p)
   :config
-  (load-theme 'doom-one t)
-
-  ;; make title bar use same style as theme on mac
-  (when (eq system-type 'darwin)
-    (setq default-frame-alist '((ns-appearance . dark)
-                                (ns-transparent-title-bar . t)))))
+  (load-theme 'doom-one t))
 
 (provide 'init-theme)
 
