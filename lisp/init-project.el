c21a10a6f69cab0968ea6ef51d59f3d171478115
Martin Polden <mpolden@mpolden.no>
2021-08-25 19:30:17 +0200

    project: Improve performance for nested Go project

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -2,6 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun mpolden/project-try-go (dir)
+  "Find root directory of a nested modules-based Go project from DIR."
+  ;; this speeds up working with a go project nested inside a large repository
+  (let* ((module-root (locate-dominating-file dir "go.mod"))
+         (is-vc-root (file-directory-p (expand-file-name ".git" module-root))))
+    (when (and module-root (not is-vc-root))
+      (cons 'transient module-root))))
+
 (use-package project
   :ensure t
   :init
@@ -11,7 +19,8 @@
                                   (project-eshell "Eshell")
                                   (mpolden/grep "Grep" ?g)
                                   (magit-project-status "Magit" ?m)))
-
+  ;; configure how projects are detected
+  (setq project-find-functions '(mpolden/project-try-go project-try-vc))
   :bind (;; C-x f finds file in project
          ("C-x f" . project-find-file)
          ;; C-c p switches project
