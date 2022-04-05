b84189264f9ca563a09ed8e9a81c7905c94494ca
Martin Polden <mpolden@mpolden.no>
2022-04-05 18:51:27 +0200

    Revert "markdown: Add helper for browsing fontified links"
    This reverts commit 6e59eea72855185edf6d3dab4085db31853b59d9.
    
    https://github.com/joaotavora/eglot/issues/865 has been fixed.

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -2,17 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun mpolden/browse-help-at-point ()
-  "Browse help URL at point.
-
-The URL is extracted from the help text found at point, if any."
-  ;; markdown-mode fontifies links by adding them to the help-echo property
-  ;; allows visiting links in documentation shown by eglot/eldoc
-  (interactive)
-  (let ((url (get-text-property (point) 'help-echo)))
-    (if url (browse-url url)
-      (error "No URL found"))))
-
 (use-package markdown-mode
   :ensure t
   :commands gfm-mode
