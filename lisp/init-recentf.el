8a3d0edb72612eec139913c382cdc7053329716e
Martin Polden <mpolden@mpolden.no>
2022-06-18 21:54:30 +0200

    recentf: exclude files in symlinked user-emacs-directory

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -2,6 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun mpolden/recentf-exclude-p (filename)
+  "Return non-nil if file FILENAME is in a symlinked `user-emacs-directory'."
+  (and (not (file-remote-p filename))
+       (file-in-directory-p filename user-emacs-directory)
+       (file-symlink-p (directory-file-name user-emacs-directory))
+       (not (equal filename (file-truename filename)))))
+
 (use-package recentf
   :init
   ;; set number of entries to save
@@ -10,6 +17,7 @@
   :config
   ;; ignore files in elpa directory as these may be opened by package.el
   (add-to-list 'recentf-exclude (expand-file-name "elpa" user-emacs-directory))
+  (add-to-list 'recentf-exclude #'mpolden/recentf-exclude-p)
   (recentf-mode 1))
 
 (provide 'init-recentf)
