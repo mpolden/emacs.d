a648c3c07abaf7cc3dbea9f25f0de5e69e4ddbe1
Martin Polden <mpolden@mpolden.no>
2022-07-03 17:43:44 +0200

    flymake: stop using obsolete function

--- lisp/init-flymake.el
+++ lisp/init-flymake.el
@@ -5,7 +5,7 @@
 (use-package flymake
   :bind (:map flymake-mode-map
               ;; C-c d l shows a list of all errors
-              ("C-c d l" . flymake-show-diagnostics-buffer)
+              ("C-c d l" . flymake-show-buffer-diagnostics)
               ;; C-c d n/p navigates to next/previous error
               ("C-c d n" . flymake-goto-next-error)
               ("C-c d p" . flymake-goto-prev-error)))
