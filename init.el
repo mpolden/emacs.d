7f3f862531fa43ed7934b3ce1bbcd873d8cd9f79
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2023-01-05 20:39:06 +0100

    Revert "rust: remove unused config"
    This reverts commit 237298c037932f9cd752beb7aa5aa743b1585455.

--- init.el
+++ init.el
@@ -59,6 +59,7 @@
 (mpolden/maybe-require 'init-markdown)
 (mpolden/maybe-require 'init-org)
 (mpolden/maybe-require 'init-python)
+(mpolden/maybe-require 'init-rust)
 (mpolden/maybe-require 'init-yaml)
 
 (provide 'init)
