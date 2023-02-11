c19eda0bd48c2c4e3ca714700217dfdc2f993af2
Martin Polden <mpolden@mpolden.no>
2023-02-11 16:37:14 +0100

    recentf: remove buggy exclusion rules

--- lisp/init-recentf.el
+++ lisp/init-recentf.el
@@ -2,20 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun mpolden/recentf-exclude-p (filename)
-  "Return non-nil if FILENAME is a symlink in the user's home directory."
-  (and (not (file-remote-p filename))
-       (not (file-in-directory-p filename org-directory))
-       (file-in-directory-p filename "~")
-       (not (equal filename (file-truename filename)))))
-
 (use-package recentf
   :init
   ;; set number of entries to save
   (setq recentf-max-saved-items 1000)
 
   :config
-  (add-to-list 'recentf-exclude #'mpolden/recentf-exclude-p)
   (recentf-mode 1))
 
 (provide 'init-recentf)
