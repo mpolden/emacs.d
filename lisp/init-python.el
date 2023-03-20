d301b09bc7f8433a4d42ea89ff10dca440f5794e
Martin Polden <mpolden@mpolden.no>
2023-03-20 18:06:39 +0100

    python: remove unused interpreter customization

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -15,16 +15,7 @@
 
 (use-package python
   :mode ("\\.py\\'" . python-mode)
-  :hook (python-mode . mpolden/python-mode-buffer-local-variables)
-  :config
-  ;; set prefered interpreter
-  (setq python-shell-interpreter (cond ((executable-find "ipython3") "ipython3")
-                                       ((executable-find "python3") "python3")
-                                       t "python"))
-  ;; ipython needs --simple-prompt for emacs compatibility
-  ;; https://emacs.stackexchange.com/a/24572
-  (when (equal python-shell-interpreter "ipython3")
-    (setq python-shell-interpreter-args "-i --simple-prompt")))
+  :hook (python-mode . mpolden/python-mode-buffer-local-variables))
 
 (provide 'init-python)
 
