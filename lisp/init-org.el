79e9ed1451ed5e0a0cfbd176f88227aba4ecd546
Martin Polden <mpolden@mpolden.no>
2023-04-04 19:10:22 +0200

    org: allow capturing task directly

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -40,6 +40,9 @@
   ;; capture template including title, date and time
   (setq org-capture-templates '(("i" "Inbox" entry (file "")
                                  "* TODO %?\n%U"
+                                 :empty-lines-after 1)
+                                ("t" "Tasks" entry (file+headline "notes.org" "Tasks")
+                                 "* TODO %?\n%U"
                                  :empty-lines-after 1)))
 
   ;; cycle opens archived trees
