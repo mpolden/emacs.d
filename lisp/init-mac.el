bdca4f9b88d58cb21c940ab283ce6887e35123b7
Martin Polden <mpolden@mpolden.no>
2023-07-24 20:59:35 +0200

    Revert "mac: remove obsolete key translation hacks"
    This reverts commit ab94fdc58f42718ddca51aae0a4ccc8bbf7b56c9.
    
    Needed for plain Emacs (something other than emacs-mac).

--- lisp/init-mac.el
+++ lisp/init-mac.el
@@ -2,8 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
-;; use command as meta
+;; configure modifiers
+(setq mac-option-modifier 'super)
 (setq mac-command-modifier 'meta)
+(setq ns-function-modifier 'hyper)
+
+;; Norwegian mac-keyboard
+(define-key key-translation-map (kbd "s-8") (kbd "["))
+(define-key key-translation-map (kbd "s-(") (kbd "{"))
+(define-key key-translation-map (kbd "s-9") (kbd "]"))
+(define-key key-translation-map (kbd "s-)") (kbd "}"))
+(define-key key-translation-map (kbd "s-7") (kbd "|"))
+(define-key key-translation-map (kbd "s-/") (kbd "\\"))
+(define-key key-translation-map (kbd "M-s-7") (kbd "M-|"))
+(define-key key-translation-map (kbd "s-¨") (kbd "~"))
 
 ;; disable native tabs
 (setq mac-frame-tabbing nil)
