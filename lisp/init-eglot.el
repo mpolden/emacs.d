8714f0ccbca62a7a62a9cd2bf72189870abe3ee1
Martin Polden <mpolden@mpolden.no>
2024-06-05 10:03:17 +0200

    Revert "eglot: add eldoc-box"
    This reverts commit cc563de47759a0223438cd6bb40adc9f036e72b6.

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -11,9 +11,6 @@
 (defvar mpolden/inhibit-lsp nil
   "List of modes where `eglot-ensure' should not be called to enable LSP integration.")
 
-(defvar mpolden/inhibit-lsp-doc nil
-  "List of modes where language documentation should not be displayed using `eldoc-box'.")
-
 (defun mpolden/major-mode-member (modes)
   "Return non-nil if `major-mode' is an element of MODES."
   (if (listp modes)
@@ -39,9 +36,7 @@
 (defun mpolden/eglot-ensure ()
   "Enable Eglot for current `major-mode'."
   (unless (mpolden/major-mode-member mpolden/inhibit-lsp)
-    (eglot-ensure)
-    (unless (mpolden/major-mode-member mpolden/inhibit-lsp-doc)
-      (eldoc-box-hover-mode))))
+    (eglot-ensure)))
 
 (use-package eglot
   :ensure t
@@ -69,9 +64,6 @@
               ("C-c o" . eglot-code-action-organize-imports)
               ;; C-c q shows code actions (quickfix)
               ("C-c q" . eglot-code-actions)))
-
-(use-package eldoc-box
-  :ensure t)
 
 (provide 'init-eglot)
 
