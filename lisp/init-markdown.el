7c93b80b1fc5986c802fd5ef4f8ed07b2b7b64b7
Martin Polden <mpolden@mpolden.no>
2025-05-07 13:05:46 +0200

    markdown: use gfm-mode for .mdx extension

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -6,8 +6,8 @@
   :ensure t
   :commands (markdown-mode gfm-mode)
   :mode
-  ;; use gfm mode for .md and .markdown extensions
-  (("\\.\\(md\\|markdown\\)\\'" . gfm-mode)
+  ;; use gfm mode for .md, .mdx and .markdown extensions
+  (("\\.\\(mdx?\\|markdown\\)\\'" . gfm-mode)
    ;; use gfm mode for pull request and issue buffers
    ("PULLREQ_EDITMSG" . gfm-mode)
    ("ISSUE_EDITMSG" . gfm-mode)))
