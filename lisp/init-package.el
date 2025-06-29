cf8f26cda73a28ac4a782527e319088f546a9a5f
Martin Polden <mpolden@mpolden.no>
2025-06-29 15:18:13 +0200

    package: remove old gnutls workaround

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -8,18 +8,6 @@
              '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; emacs and gnutls has recurring issues with tls 1.3
-;; the original bug was supposedly fixed in 26.3, but resurfaced in 27.2 on
-;; macos (https://emacsformacosx.com build)
-;;
-;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
-;; https://www.reddit.com/r/emacs/comments/mk0luk/does_anyone_know_how_to_diagnose_or_fix_emacs_not/
-;;
-;; disable tls 1.3 if supported by gnutls
-(when (and (< emacs-major-version 28)
-           (>= libgnutls-version 30603))
-  (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
-
 (defun mpolden/require-package (package)
   "Install given PACKAGE if it's not already installed."
   (unless (package-installed-p package)
