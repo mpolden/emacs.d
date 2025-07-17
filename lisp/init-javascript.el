0d05d661685e63b9ec1752c30eb1a13158fb2551
Martin Polden <mpolden@mpolden.no>
2025-07-17 18:37:58 +0200

    javascript: use js-json-mode for json

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -8,8 +8,8 @@
   (setopt js-indent-level 2)
 
   :mode
-  ;; use js-mode for json and jsonc
-  ("\\.jsonc?\\'" . js-mode)
+  ;; use js-json-mode for json and jsonc
+  ("\\.jsonc?\\'" . js-json-mode))
 
   :bind (:map js-mode-map
               ;; C-c f formats json
