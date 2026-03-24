4fd1d123b6441b3a94d7c93eb285b0e55a880f6b
Martin Polden <mpolden@mpolden.no>
2026-03-24 20:11:57 +0100

    gptel: remove unused configuration

--- init.el
+++ init.el
@@ -45,7 +45,6 @@
 (mpolden/maybe-require 'init-eglot)
 (mpolden/maybe-require 'init-eshell)
 (mpolden/maybe-require 'init-git)
-(mpolden/maybe-require 'init-gptel)
 
 ;; languages
 (mpolden/maybe-require 'init-fish)
